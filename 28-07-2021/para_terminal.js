e10889e77d4bf8870177cefae01a25c8c25ad26e
Gopesh <42885099+gopesh26@users.noreply.github.com>
2021-08-01 20:44:49 +0530

    Prompt from terminal

---
+++ 28-07-2021/para_terminal.js
@@ -0,0 +1,6 @@
+const ip =require("prompt-sync");
+const prompt = ip();
+let num1 = Number(prompt("enter num1:"));
+let num2 = Number(prompt("enter num2:"));
+let sum = (num1+num2);
+console.log(sum);
