6030c79603e79394a99344c46a0231b670946f28
Gopesh <42885099+gopesh26@users.noreply.github.com>
2021-08-03 00:51:50 +0530

    Update Form_validation.js

--- 30-07-2021/Form_validation.js
+++ 30-07-2021/Form_validation.js
@@ -1,7 +1,7 @@
 const express =require("express");
 const {check, validationResult} = require('express-validator/check');
 
-const{matchedData ,sanitizedBody} = require('express-validator/filter')
+const{matchedData ,sanitizedBody} = require('express-validator/filter');
 const app = express(); 
  
 
@@ -14,56 +14,59 @@ app.use(express.urlencoded());
 app.use(express.json());
 
 app.set('view engine','twig');
+
 app.set('views','./public/views');
 
 app.get("/",(req,res)=>{
-    res.render('form',{title:"Form",messaage:"Fill the form"});
+    res.render('form',{title:"Sign up",messaage:"Fill the form"});
 });
 
 app.post("/",[
 check('name','Name Required')
-.isString()
-.notEmpty()
-.withMessage('Name Required'),
+.notEmpty().withMessage("Should not be empty")
+.matches(/^[a-zA-Z]+$/).withMessage('Only characters'),
 
 check('mobile','Mobile Number Required ')
-.isLength({min:10})
-.notEmpty(),
+.isLength({min:10}).withMessage("Enter 10 Digit mobile Number")
+.isMobilePhone('en-IN')
+.matches(/[89][0-9]{9}/).withMessage("Should start with 8,9")
+.notEmpty().withMessage("Should not be empty"),
 
-check('email','Email required')
-.notEmpty()
-.isEmail(),
+check('email')
+.notEmpty().withMessage("Email Should not be empty")
+.isEmail().withMessage("Must be a valid email"),
 
 check('password','Please enter password with min 8 characters ')
-.notEmpty()
-.withMessage('password is required')
-.isLength({ min: 8 })
-.withMessage('password must be 8 characters'),
+.notEmpty().withMessage("should not be empty")
+.isLength({ min: 8 }).withMessage('Password must be of min 8 char'),
 
-check('adds','Please enter Address ')
+check('confirm_pass').custom((value, { req }) => {
+    //console.log(value);
+    if (value !== req.body.password) {
+          throw new Error('Password Confirmation does not match password');
+     }
+     return true;
+}).withMessage('password does not match'),
+
+check('adds','Address Required')
 .notEmpty()
-.withMessage('Address Required')
 ]
 ,(req,res)=>{
     const errors = validationResult(req);
     console.log(errors.mapped());
     if(!errors.isEmpty()){
-        res.render('form',{title:"Form error",error:errors.mapped()});
+        const user = matchedData(req);
+        res.render('form',{title:"Sign up",error:errors.mapped(),user:user});
     }
     else{
         const user = matchedData(req);
-        res.render('form_data',{title:"Form Data",name:req.body.name,
-         mobile:req.body.mobile,email:req.body.email,
-          password:req.body.password,adds:req.body.adds,birthday:req.body.birthday});
+        res.render('form_data',{title:"Form Data",user:user});
     }
     
     
 });
 
 app.listen(3080);
-
-
-
 
 
 // res.render('form_data',{title:"Form Data",name:req.body.name,
