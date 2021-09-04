dbbaad4e21ba10c3dfe35592ff434e0ef384024d
Gopesh <42885099+gopesh26@users.noreply.github.com>
2021-09-04 21:35:47 +0530

    app

---
+++ 1-09-21/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+// import axios from 'axios';
+// import File1 from './file1';
+import Counter from '../src/components/counter'
+
+
+function App() {
+
+    return (
+        <Counter></Counter>
+    );
+}
+
+export default App;
