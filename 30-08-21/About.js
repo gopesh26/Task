53815275191b365e6fbb38b0eb80e3eea69cbdd8
Gopesh <42885099+gopesh26@users.noreply.github.com>
2021-09-06 08:32:04 +0530

    Router hooks

---
+++ 30-08-21/About.js
@@ -0,0 +1,16 @@
+
+import {useLocation} from "react-router-dom";
+  
+  
+export default function ( props ){
+      
+  // Accessing the location and history 
+  // through the props
+  const location = props.location;
+  const history = props.history;
+  return(
+    <div>
+       // Some content
+    </div>
+  );
+}
